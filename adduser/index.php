b66e8eba8eb911d18dbe97ca0f18e0da852b39ef
Kieran <kieranharrigan@gmail.com>
2017-04-03 15:40:11 -0400

    Removed verification email for testing.

--- adduser/index.php
+++ adduser/index.php
@@ -4,54 +4,52 @@ $username = $fields['username'];
 $password = $fields['password'];
 $email = $fields['email'];
 
-if ($username !== NULL && $password !== NULL && $email !== NULL) :
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $phrase = 'Invalid email address.';
-    }
-    else {
-$cluster = Cassandra::cluster()->build();
-$keyspace = 'twitter';
-$session = $cluster->connect($keyspace);
-$statement = new Cassandra\SimpleStatement(
-    "SELECT * FROM twitter.users WHERE username='" . strtolower($username) . "'"
-);
-$future = $session->executeAsync($statement);
-$result = $future->get();
+if ($username !== NULL && $password !== NULL && $email !== NULL):
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$phrase = 'Invalid email address.';
+	} else {
+		$cluster = Cassandra::cluster()->build();
+		$keyspace = 'twitter';
+		$session = $cluster->connect($keyspace);
+		$statement = new Cassandra\SimpleStatement(
+			"SELECT * FROM twitter.users WHERE username='" . strtolower($username) . "'"
+		);
+		$future = $session->executeAsync($statement);
+		$result = $future->get();
 
-    if($result->first() === NULL) {
-        $key = md5(uniqid($username, true));
+		if ($result->first() === NULL) {
+			$key = md5(uniqid($username, true));
 
-$batch     = new Cassandra\BatchStatement(Cassandra::BATCH_LOGGED);
+			$batch = new Cassandra\BatchStatement(Cassandra::BATCH_LOGGED);
 
-$statement = new Cassandra\SimpleStatement(
-    "INSERT INTO twitter.users (username,password,disabled,email,key,followers,following) VALUES ('" . strtolower($username) . "','" . $password . "',true,'" . $email . "','" . $key . "','{\"followers\":[]}','{\"following\":[]}')"
-);
+			$statement = new Cassandra\SimpleStatement(
+				"INSERT INTO twitter.users (username,password,disabled,email,key,followers,following) VALUES ('" . strtolower($username) . "','" . $password . "',true,'" . $email . "','" . $key . "','{\"followers\":[]}','{\"following\":[]}')"
+			);
 
-$emails = new Cassandra\SimpleStatement(
-    "INSERT INTO twitter.emails (username,password,disabled,email,key) VALUES ('" . strtolower($username) . "','" . $password . "',true,'" . $email . "','" . $key . "')"
-);
+			$emails = new Cassandra\SimpleStatement(
+				"INSERT INTO twitter.emails (username,password,disabled,email,key) VALUES ('" . strtolower($username) . "','" . $password . "',true,'" . $email . "','" . $key . "')"
+			);
 
-$batch->add($emails);
-$batch->add($statement);
+			$batch->add($emails);
+			$batch->add($statement);
 
-$session->execute($batch);
-$session->closeAsync();
+			$session->execute($batch);
+			$session->closeAsync();
 
-        $phrase = 'OK';
+			$phrase = 'OK';
 
-        $body = "Thank you for creating an account with Twitter Clone.\r\n\r\n" . "Username: " . strtolower($username) . "\r\nKey: " . $key . "\r\n\r\nPlease click the following link to verify your email:\r\nhttp://kiharrigan.cse356.compas.cs.stonybrook.edu/verify/verify.php?email=" . $email . "&key=" . $key;
-            
-            exec("php send.php '$email' '$body' > /dev/null 2>&1 &");
-     }
-     else {
-         $phrase = 'ERROR';
-     }
-}
+			$body = "Thank you for creating an account with Twitter Clone.\r\n\r\n" . "Username: " . strtolower($username) . "\r\nKey: " . $key . "\r\n\r\nPlease click the following link to verify your email:\r\nhttp://kiharrigan.cse356.compas.cs.stonybrook.edu/verify/verify.php?email=" . $email . "&key=" . $key;
+
+//			exec("php send.php '$email' '$body' > /dev/null 2>&1 &");
+		} else {
+			$phrase = 'ERROR';
+		}
+	}
 	$response = array("status" => $phrase);
 	$json = json_encode($response);
 
 	echo $json;
-else :
+else:
 ?>
 <html>
 <head>
