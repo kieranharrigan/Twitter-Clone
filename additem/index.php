abe3782785bed9048827586660eb00bfeef61e98
--get <--get>
2017-04-05 22:37:11 -0400

    Ensure client gets response on additem.

--- additem/index.php
+++ additem/index.php
@@ -5,53 +5,37 @@ $fields = json_decode(file_get_contents('php://input'), true);
 $content = $fields['content'];
 
 if ($content !== NULL && $_SESSION['username'] !== NULL):
-	if (false) {
-	}
-//    if(strlen($content) > 140) {
-	//        $phrase = 'ERROR';
-	//    }
-else {
-		$local = Cassandra::cluster()->build();
-		$keyspace = 'twitter';
-		$local_sess = $local->connect($keyspace);
+	$local = Cassandra::cluster()->build();
+	$keyspace = 'twitter';
+	$local_sess = $local->connect($keyspace);
 
-		$cluster = Cassandra::cluster()->withContactPoints('192.168.1.13')->build();
-		$keyspace = 'twitter';
-		$session = $cluster->connect($keyspace);
+	$cluster = Cassandra::cluster()->withContactPoints('192.168.1.13')->build();
+	$keyspace = 'twitter';
+	$session = $cluster->connect($keyspace);
 
-		$id = md5(uniqid($_SESSION['username'], true));
+	$id = md5(uniqid($_SESSION['username'], true));
 
 //$results_file = fopen('results.txt', 'a');
-		//fwrite($results_file, strval($id) . ': "' . $content . '"' . PHP_EOL);
-		//fclose($results_file);
+	//fwrite($results_file, strval($id) . ': "' . $content . '"' . PHP_EOL);
+	//fclose($results_file);
 
-		$batch = new Cassandra\BatchStatement(Cassandra::BATCH_LOGGED);
+	$batch = new Cassandra\BatchStatement(Cassandra::BATCH_LOGGED);
 
-		$escape = str_replace("'", "''", $content);
+	$escape = str_replace("'", "''", $content);
 
-		$insertByTime = new Cassandra\SimpleStatement(
-			"INSERT INTO tweets (id,content,sort,timestamp,username) VALUES ('" . strval($id) . "','" . $escape . "',1," . time() . ",'" . strtolower($_SESSION['username']) . "')"
-		);
+	$insertByTime = new Cassandra\SimpleStatement(
+		"INSERT INTO tweets (id,content,sort,timestamp,username) VALUES ('" . strval($id) . "','" . $escape . "',1," . time() . ",'" . strtolower($_SESSION['username']) . "')"
+	);
 
-		$insertById = new Cassandra\SimpleStatement(
-			"INSERT INTO tweetsbyid (id,content,sort,timestamp,username) VALUES ('" . strval($id) . "','" . $escape . "',1," . time() . ",'" . strtolower($_SESSION['username']) . "')"
-		);
+	$insertById = new Cassandra\SimpleStatement(
+		"INSERT INTO tweetsbyid (id,content,sort,timestamp,username) VALUES ('" . strval($id) . "','" . $escape . "',1," . time() . ",'" . strtolower($_SESSION['username']) . "')"
+	);
 
-		$insertByUn = new Cassandra\SimpleStatement(
-			"INSERT INTO tweetsbyun (id,content,sort,timestamp,username) VALUES ('" . strval($id) . "','" . $escape . "',1," . time() . ",'" . strtolower($_SESSION['username']) . "')"
-		);
+	$insertByUn = new Cassandra\SimpleStatement(
+		"INSERT INTO tweetsbyun (id,content,sort,timestamp,username) VALUES ('" . strval($id) . "','" . $escape . "',1," . time() . ",'" . strtolower($_SESSION['username']) . "')"
+	);
 
-		$batch->add($insertByTime);
-		$batch->add($insertById);
-		$batch->add($insertByUn);
-
-		$local_sess->executeAsync($batch);
-		$local_sess->closeAsync();
-
-		$session->executeAsync($batch);
-		$session->closeAsync();
-		$phrase = 'OK';
-	}
+	$phrase = 'OK';
 	$response = array("status" => $phrase);
 
 	if (strcmp($phrase, 'OK') === 0) {
@@ -60,6 +44,17 @@ else {
 	$json = json_encode($response);
 
 	echo $json;
+
+	$batch->add($insertByTime);
+	$batch->add($insertById);
+	$batch->add($insertByUn);
+
+	$local_sess->execute($batch);
+	$local_sess->closeAsync();
+
+	$session->execute($batch);
+	$session->closeAsync();
+
 elseif ($_SESSION['username'] === NULL):
 	$response = array("status" => "error");
 	$response['error'] = 'You must be logged in before you can compose tweets.';
