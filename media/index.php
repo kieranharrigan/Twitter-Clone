faaf7f276372ffaf0919c8fb44850028d012bc08
--get <--get>
2017-04-15 02:43:44 -0400

    Retrieve media at /media/<id>. Modified htaccess.

---
+++ media/index.php
@@ -0,0 +1,23 @@
+<?php
+//header('Content-Type: image/jpeg');
+
+$id = $_GET['id'];
+//$fields = json_decode(file_get_contents('php://input'), true);
+//$filename = $fields['filename'];
+
+$cluster = Cassandra::cluster()->build();
+$keyspace = 'twitter';
+$session = $cluster->connect($keyspace);
+
+$statement = new Cassandra\SimpleStatement(
+	"SELECT content FROM media WHERE id='" . $id . "'"
+);
+$future = $session->executeAsync($statement);
+$result = $future->get();
+
+foreach ($result as $row) {
+	$hex = substr($row['content'], 2);
+	$binary = pack("H*", $hex);
+	echo base64_decode($binary);
+}
+?>
