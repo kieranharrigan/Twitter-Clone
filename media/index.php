98f3ed09a0a2380f867d9d607130417e6f1542f3
--get <--get>
2017-04-19 22:01:23 -0400

    Invalid media ID returns error.

--- media/index.php
+++ media/index.php
@@ -1,6 +1,4 @@
 <?php
-header('Content-Type: image/jpeg');
-
 $id = $_GET['id'];
 //$fields = json_decode(file_get_contents('php://input'), true);
 //$filename = $fields['filename'];
@@ -16,9 +14,18 @@ $future = $session->executeAsync($statement);
 $result = $future->get();
 $session->closeAsync();
 
-foreach ($result as $row) {
-	$hex = substr($row['content'], 2);
-	$binary = pack("H*", $hex);
-	echo base64_decode($binary);
+if ($result->first() !== NULL) {
+	header('Content-Type: image/jpeg');
+
+	foreach ($result as $row) {
+		$hex = substr($row['content'], 2);
+		$binary = pack("H*", $hex);
+		echo base64_decode($binary);
+	}
+} else {
+	$response = array("status" => "error");
+	$response['error'] = 'No media found with id: ' . $id;
+	$json = json_encode($response);
+	echo $json;
 }
 ?>
