d3d826e94bd2bc1905af5b1e03af9045aa845827
--get <--get>
2017-05-10 19:21:34 -0400

    Bootstrapped search page.

--- search/index.php
+++ search/index.php
@@ -267,18 +267,92 @@ else:
 <html>
 <head>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script type="text/javascript" src="/search/search.js"></script>
 </head>
 
 <body>
-    <form id="input" onsubmit="event.preventDefault(); passToAdd();" autocomplete="off">
-        Tweets from this time and earlier: <input type="text" name="timestamp" value="<?php echo time() ?>" autofocus onfocus="this.value = this.value;"><br>
-        Maximum number of results: <input type="text" name="limit" value="25"><br>
-        Search term: <input type="text" name="q"><br>
-        Only show tweets by this user: <input type="text" name="username"><br>
-        Only show tweets by users you follow: <input type="checkbox" name="following" value="following" checked><br>
-        <input type="submit" value="search">
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand">Twitter Clone</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="/additem">New Tweet</a></li>
+        <li><a href="/addmedia">Add Media</a></li>
+        <li class="active"><a href="/search">Search <span class="sr-only">(current)</span></a></li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+		<?php if ($_SESSION['username'] === NULL): ?>
+		<li><a href="/adduser">Create Account</a></li>
+        <li><a href="/login">Login</a></li>
+		<?php else: ?>
+		<li><a>Hello, <?php echo $_SESSION['username']; ?></a></li>
+	    <li><a href="/logout">Logout</a></li>
+	<?php endif;?>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+    <form id="input" onsubmit="event.preventDefault(); passToAdd();" autocomplete="off" class="form-horizontal">
+    	<div class="form-group">
+			<label class="control-label col-sm-2">Tweets from this time and earlier:</label>
+			<div class="col-sm-10">
+				<input class="form-control" type="text" name="timestamp" value="<?php echo time() ?>" autofocus onfocus="this.value = this.value;">
+			</div>
+		</div>
+    	<div class="form-group">
+			<label class="control-label col-sm-2">Maximum number of results:</label>
+			<div class="col-sm-10">
+				<input class="form-control" type="text" name="limit" value="25">
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="control-label col-sm-2">Search term:</label>
+			<div class="col-sm-10">
+				<input class="form-control" type="text" name="q">
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="control-label col-sm-2">Only show tweets by this user:</label>
+			<div class="col-sm-10">
+				<input class="form-control" type="text" name="username">
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="control-label col-sm-2">Only show tweets by users you follow:</label>
+			<div class="col-sm-10">
+				<input class="form-control" type="checkbox" name="following" value="following" checked>
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="btn btn-default">Submit</button>
+			</div>
+		</div>
     </form>
+</div>
 
     <div id="result"></div>
 </body>
