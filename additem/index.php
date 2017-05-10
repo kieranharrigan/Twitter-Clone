dd618239cf90b8945819e38b16b47ad85ad36b7f
--get <--get>
2017-05-09 20:57:13 -0400

    Bootstrapped additem page.

--- additem/index.php
+++ additem/index.php
@@ -172,15 +172,69 @@ else:
 <html>
 <head>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <script type="text/javascript" src="/additem/additem.js"></script>
 </head>
 
 <body>
-    <form id="input" oninput="updateCount();" onsubmit="event.preventDefault(); passToAdd();" autocomplete="off">
-        <textarea id="tweet" type="text" name="content" maxlength="140" rows="6" cols="50" style="resize:none" autofocus></textarea><br>
-        Characters left: <span id="rem">140</span><br>
-        Parent ID: <input type="text" name="parent"><br>
-        Media IDs (comma separated): <input type="text" name="media">
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Twitter Clone</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="/additem">New Tweet <span class="sr-only">(current)</span></a></li>
+        <li><a href="/addmedia">Add Media</a></li>
+        <li><a href="/search">Search</a></li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+		<?php if ($_SESSION['username'] === NULL): ?>
+		<li><a href="/adduser">Create Account</li>
+        <li><a href="/login">Login</a></li>
+		<?php else: ?>
+		<li><a>Hello, <?php echo $_SESSION['username']; ?></a></li>
+	    <li><a href="/logout">Logout</a></li>
+	<?php endif;?>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+    <form id="input" oninput="updateCount();" onsubmit="event.preventDefault(); passToAdd();" autocomplete="off" class="form-horizontal">
+    	<div class="form-group">
+        	<textarea class="form-control" id="tweet" type="text" name="content" maxlength="140" rows="6" cols="50" style="resize:none" autofocus></textarea>
+        	Characters left: <span id="rem">140</span>
+        </div>
+        <div class="form-group">
+        	<label class="control-label col-sm-2">Parent:</label>
+			<div class="col-sm-10">
+				<input class="form-control" type="text" name="parent" placeholder="Enter parent tweet ID">
+			</div>
+        </div>
+        <div class="form-group">
+        	<label class="control-label col-sm-2">Media:</label>
+			<div class="col-sm-10">
+        		<input class="form-control" type="text" name="media" placeholder="Enter media IDs (comma separated)">
+        	</div>
+        </div>
         <input type="submit" value="submit">
     </form>
 
